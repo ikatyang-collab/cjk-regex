a10cefbfa9678a3ee9001869bd8e37eb78f0fab8
fisker <lionkay@gmail.com>
2025-03-10 21:38:09 +0800

    test: snapshot data

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -128,3 +128,9 @@ Object.keys(testCases).forEach(character => {
     }
   })
 })
+
+for (const [name, getter] of Object.entries(cjk)) {
+  test(`Snapshot: ${name}`, () => {
+    expect(getter().data).matchSnapshot()
+  })
+}
